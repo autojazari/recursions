e9150677c4f61006ef572743c7e70a6b6d0f2f34
Sameh Mohamed <sameh@autojazari.com>
2017-03-26 10:52:27 -0400

    binary search implemented and elfish began

---
+++ binary_search.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Sam Mohamed
+Binary search implementation
+
+Usage:
+    $ python3 -m doctest binary_search.py
+"""
+def search(array, item):
+    """
+    >>> search([], 1)
+    False
+
+    >>> search([1,2,3,4], 8)
+    False
+
+    >>> search([1,2,3,4], 0)
+    False
+
+    >>> search([1,2,3,5], 4)
+    False
+
+    >>> search([1,2,3,5], 3)
+    True
+
+    >>> search([1,2,3,5], 5)
+    True
+
+    >>> search([1,2,3,5], 1)
+    True
+
+    """
+
+    if not array:
+        return False
+
+    if len(array) == 1:
+        return array[0] == item
+
+    mid = len(array) // 2
+    left = array[:mid]
+    right = array[mid:]
+
+    return search(left, item) or search(right, item)
---
+++ elfish.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Sam Mohamed
+A word is considered elfish if it contains the
+letters: e, l, and f in it, in any order. For example, we would say
+that the following words are elfish: whiteleaf, tasteful, unfriendly,
+and waffles, because they each contain those letters
+
+Usage:
+    $ python3 -m doctest elfish.py
+"""
+def elfish(word):
+	"""
+    >>> elfish('')
+    False
+
+    >>> elfish('whiteleaf')
+    True
+
+    >>> elfish('whiteleaf')
+    True
+
+    >>> elfish('unfriendly')
+    True
+
+    >>> elfish('waffles')
+    True
+
+    >>> elfish('waffles')
+    True
+
+    >>> elfish('sameh')
+    False
+
+    >>> elfish('wolf')
+    False
+
+    """
+	if len(word) == 1:
+		return word == 'e' or word == 'l' or word == 'f'
